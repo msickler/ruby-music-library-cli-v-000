583ef528d9e4fd7fe9fae0e69ba96aa575790680
Mariana Sickler <marianaleal_93@hotmail.com>
2017-12-19 15:07:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,9 @@ class Song
   attr_accessor :name, :artist
   @@all = []
 
-  def initialize(name)
+  def initialize(name, artist = nil)
     @name = name
+    @artist = artist
   end
 
   def self.name
