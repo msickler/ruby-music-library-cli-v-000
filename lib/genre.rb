daf6dd3564cce6f1358e0005eccb3372f7786692
Mariana Sickler <marianaleal_93@hotmail.com>
2017-12-19 15:02:47 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,29 +1,29 @@
 class Genre
   attr_accessor :name
   @@all = []
-  
-  def initialize(name)
-    @name = name 
-    @@all << self 
-  end 
 
-  def self.all 
-    @@all 
-  end 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
   def self.destroy_all
     @@all.clear
-  end 
+  end
 
   def save
-    @@all << self 
-  end 
+    @@all << self
+  end
 
   def self.create(name)
     genre = Genre.new(name)
     genre.save
-    genre 
-  end 
+    genre
+  end
 
-  
+
 end
