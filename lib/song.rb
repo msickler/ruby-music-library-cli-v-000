ccddbf51936e9d3ee46634ef57316958017cfc8f
Mariana Sickler <marianaleal_93@hotmail.com>
2017-12-19 14:57:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,29 +1,32 @@
 class Song
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
-    @@all = []
   end
 
-def self.name
-  @name
-end
+  def self.name
+    @name
+  end
 
-def self.all
-  @@all
-end
+  def self.all
+    @@all
+  end
 
-def self.destroy_all
-  @@all.destroy
-end
+  def self.destroy_all
+    @@all.clear
+  end
 
-def self.save
-  @@all << self
-end
+  def self.save
+    @@all << self
+  end
 
-def create
-end
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end
 
 
 end
