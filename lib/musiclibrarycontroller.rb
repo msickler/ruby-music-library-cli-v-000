9b47bf290cdc1edac970b89b9f70475994b5e1a7
Mariana Sickler <marianaleal_93@hotmail.com>
2017-12-19 15:56:34 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -10,16 +10,27 @@ class MusicLibraryController
     input = ""
     puts "Welcome to your music library!"
     until input == "exit"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    input = gets.strip
-  end
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      input = gets.strip
+    end
+    if input == "list songs"
+      self.list_songs
+    elsif input == "list artists"
+      self.list_artists
+    elsif input == "list genres"
+      self.list_genres
+    elsif input == "list artist"
+      self.list_songs_by_artist
+    elsif input == "list genre"
+      self.list_songs_by_genre
+    end 
   end
 
 
