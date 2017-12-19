f50e911d62e33690b7f9aca24e59666c5d022051
Mariana Sickler <marianaleal_93@hotmail.com>
2017-12-19 16:04:48 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -46,4 +46,10 @@ class MusicLibraryController
     artists = artists.sort_by {|n| n.name}
     artists.each_with_index {|artist, index| puts "#{index + 1}. #{artist.name}"}
   end
+
+  def list_genres
+    genres = Genre.all
+    genres = genres.sort_by {|n| n.name}
+    genres.each_with_index {|genre, index| puts "#{index + 1}. #{genre.name}"}
+  end
 end
