c90c02f1571d8e352dea8db57fe7ab5c66bb476b
Mariana Sickler <marianaleal_93@hotmail.com>
2017-12-19 15:29:49 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,15 @@
 module Concerns::Findable
-  
+
   def self.find_by_name(name)
     self.all.detect {|x| x.name == name}.uniq
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else 
+      self.create(name)
+    end 
   end 
+  
 end
